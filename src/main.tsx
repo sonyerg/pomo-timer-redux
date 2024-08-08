afcc10180eb910792b58a26e53f099e4c19f8343
sonyerg <ericksonrguinto@gmail.com>
2024-08-08 21:01:33 +1200

    importing changes

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 
 import "./index.css";
-import App from "./App.tsx";
-import store from "./store/index.ts";
+import App from "./App";
+import store from "./store/index";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
